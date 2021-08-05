69d7879b2c0e44d4ef05dd1f4c545d5b8626cf88
Ole Nascimento <olenascimento@gmail.com>
2021-08-05 22:42:15 +0200

    test

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv'
 dotenv.config()
-export const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/myFirstDatabase'
+export const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/db'
 export const port = process.env.PORT || 4000
 export const secret = process.env.SECRET || 'shhhh its a secret'
