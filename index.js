520378ad18ff278f5aea1f1f4ad61b8fcbb011c1
Ole Nascimento <olenascimento@gmail.com>
2021-08-05 18:30:04 +0200

    deployment prep step yarn dev

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 import express from 'express'
+import { port } from '.config/environment.js'
+import connectToDatabase from './lib/connectToDb.js'
+import logger from './lib/logger.js'
+import errorHandler from './lib/errorHandler.js'
+import path from 'path'
 import mongoose from 'mongoose'
 // import Drink from './models/drink.js'
-import { dbURI, port  } from './config/environment.js'
+import { dbURI } from './config/environment.js'
 import router from './config/router.js'
 
 const app = express()
 
+const __dirname = path.resolve()
+
 // setup the server
 const startServer = async () => {
   try {
+    await connectToDatabase
     await mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
     console.log('🚀 Database has connected successfully')
 
@@ -17,14 +25,16 @@ const startServer = async () => {
       console.log(`🚨 Incoming request: METHOD: ${req.method}, URL: ${req.url}`)
       next()
     })
-
+    app.use(express.static(`${__dirname}/client/build`))
     app.use(express.json())
-
+    app.use(logger)
     app.use('/api', router)
-
+    app.use('/*', (_, res) => res.sendFile(`${__dirname}/client/build/index.html`))
+    app.use(errorHandler)
     app.listen(port, () => console.log(`🚀 Express is up and running on port ${port}`))
     
   } catch (err) {
+   
     console.log(err, 'something has gone wrong')
   }
 }
