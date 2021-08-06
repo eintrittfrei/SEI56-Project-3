976a8060f30723c70d1def9bb519d0adab555fa1
Ole Nascimento <olenascimento@gmail.com>
2021-08-06 14:41:51 +0200

    test

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv'
 dotenv.config()
-export const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/drinks-api'
+
 export const port = process.env.PORT || 4000
+export const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/drinks-api'
 export const secret = process.env.SECRET || 'our little secret'
